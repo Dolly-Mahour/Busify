18b958066de75566b67250202ec95256c0fac655
Dolly-Mahour <dollymahour282@gmail.com>
2026-05-09 15:47:09 +0530

    navbar fixes done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,229 +1,254 @@
 import Logo from "./Logo";
 import "./Navbar.css";
-class FlagList {
-  constructor(name, imgName) {
-    this.name = name;
-    this.imgName = imgName;
-  }
-}
+import NavbarItems from "./NavbarItems";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
 export default function Navbar() {
-  const flags = [
-    new FlagList("Hindi", "../images/flag.png"),
-    new FlagList("English", "../images/united-kingdom.png"),
-    new FlagList("Japanese", "../images/japan.png"),
-    new FlagList("French", "../images/france.png"),
-    new FlagList("Spanish", "../images/spain.png"),
-  ];
   return (
     <>
       <nav className="navbar navbar-expand-lg w-100">
-        <div className="container-fluid g-0 px-4 py-2  d-flex justify-content-between align-items-center">
-          <div className="w-50">
-            <a className="navbar-brand d-flex alig-items-center" href="#">
+        <div className="container-fluid g-0 px-4 d-flex justify-content-between align-items-center">
+          <div className="d-inline-block">
+            <a className="d-flex alig-items-center" href="#">
               <Logo></Logo>
             </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
           </div>
-          <div className="w-50" id="navbarSupportedContent">
-            <ul className="navbar-nav justify-content-between d-flex align-items-center">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle d-flex align-items-center justify-content-between"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img className="h-20px" src="../images/download.png" alt="" />
-                  <p className="align-items-center d-flex ms-2">Download</p>
-                </a>
-                <div className="dropdown-menu downloadDropdown rounded-4 p-2">
-                  <div className="row g-0 h-100 w-100">
-                    <div className="col-6 h-100 d-flex flex-column p-2  justify-content-center">
-                      <p className="mb-2">Download App</p>
-                      <div className="d-flex flex-column justify-content-between h-60">
-                        <img
-                          className="h-30px w-100"
-                          src="../images/as.png"
-                          alt=""
-                        />
-                        <img
-                          className="h-30px w-100"
-                          src="../images/ps.png"
-                          alt=""
-                        />
+          <div className="w-50">
+            <ul className="w-100 d-flex align-items-center">
+              <div className="d-md-flex d-none w-50 justify-content-around align-items-center">
+                <NavbarItems></NavbarItems>
+              </div>
+              <div className="flex-grow-1 d-flex justify-content-around align-items-center">
+                <li className="nav-item">
+                  <button
+                    className="nav-link btn "
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#registerModal"
+                  >
+                    <p>Register</p>
+                  </button>
+                  <div
+                    className="modal fade"
+                    id="registerModal"
+                    tabIndex="-1"
+                    aria-labelledby="registerModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog modal-dialog-centered">
+                      <div className="modal-content p-3 rounded-4">
+                        <div className="modal-body">
+                          <div className="center-div">
+                            <Logo></Logo>
+                          </div>
+                          <h3 className="center-div fst-italic py-2">
+                            Register
+                          </h3>
+                          <DotLottieReact
+                            src="https://lottie.host/d1bdc022-e4d8-40fe-9904-fbe0265d41a0/druqOOEkd4.json"
+                            loop
+                            autoplay
+                            className="my-3"
+                          />
+                          <div className="d-flex justify-content-around align-items-center">
+                            <p>User</p>
+                            <p>Agent</p>
+                          </div>
+                          <div className="px-3">
+                            <input
+                              type="text"
+                              className="border border-secondary w-100 h-50px rounded-4 my-2 p-2 px-3"
+                              placeholder="Enter your name"
+                            />
+                            <input
+                              type="text"
+                              className="border border-secondary w-100 h-50px rounded-4 my-2 p-2 px-3"
+                              placeholder="Enter your email"
+                            />
+                            <input
+                              type="text"
+                              className="border border-secondary w-100 h-50px rounded-4 my-2 p-2 px-3"
+                              placeholder="Enter your number"
+                            />
+                            <input
+                              type="text"
+                              className="border border-secondary w-100 h-50px rounded-4 my-2 p-2 px-3"
+                              placeholder="Enter your password"
+                            />
+                            <input
+                              type="text"
+                              className="border border-secondary w-100 h-50px rounded-4 my-2 p-2 px-3"
+                              placeholder="Enter your refferal code (Optional)"
+                            />
+                          </div>
+                          <div className="center-div">
+                            <button className="btn btn-primary rounded-4 px-3 p-2 my-3">
+                              Register now
+                            </button>
+                          </div>
+                          <div className="d-flex justify-content-center my-3">
+                            already have an account?{" "}
+                            <p className="text-primary">Login</p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="col-6 d-flex flex-column p-2  justify-content-center">
-                      <p className="mb-2">Scan Code</p>
-                      <img
-                        className="h-60"
-                        src="../images/dummyQR.png"
-                        alt="download"
-                      />
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <button
+                    className="nav-link btn "
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#LoginModal"
+                  >
+                    <p>Login</p>
+                  </button>
+                  <div
+                    className="modal fade"
+                    id="LoginModal"
+                    tabIndex="-1"
+                    aria-labelledby="LoginModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog modal-dialog-centered">
+                      <div className="modal-content p-3 rounded-4">
+                        <div className="modal-body">
+                          <div className="center-div">
+                            <Logo></Logo>
+                          </div>
+                          <h3 className="center-div fst-italic py-2">Lgoin</h3>
+                          <DotLottieReact
+                            src="https://lottie.host/14273cfb-afce-4097-97ba-e66ec31c8b2b/jkgwm3q4Hl.json"
+                            loop
+                            autoplay
+                            className="my-3"
+                          />
+                          <div className="px-3">
+                            <input
+                              type="text"
+                              className="border border-secondary w-100 h-50px rounded-4 my-2 p-2 px-3"
+                              placeholder="Enter your phone number"
+                            />
+                            <input
+                              type="password"
+                              className="border border-secondary w-100 h-50px rounded-4 my-2 p-2 px-3"
+                              placeholder="Enter your password"
+                            />
+                          </div>
+                          <div className="center-div">
+                            <button className="btn btn-primary rounded-4 px-3 p-2 my-3">
+                              Lgoin now
+                            </button>
+                          </div>
+                          <div className="d-flex justify-content-center my-3">
+                            Do not have an account ?
+                            <p className="text-primary ms-2">Register</p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle d-flex align-items-center justify-content-between"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img className="h-20px" src="../images/earth.png" alt="" />
-                  <p className="align-items-center d-flex ms-2">Languages</p>
-                </a>
-                <ul className="dropdown-menu">
-                  {flags.map((flag, index) => (
+                </li>
+
+                <li className="nav-item dropdown center-div">
+                  <a
+                    className="nav-link dropdown-toggle p-0 center-div"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <img
+                      className="h-60px"
+                      src="../images/hamburger.png"
+                      alt="menu"
+                    />
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-end">
                     <li>
-                      <a className="dropdown-item d-flex  align-items-center" href="#">
-                        <img
-                        className="h-20px me-3"
-                        src={flag.imgName}
-                        alt=""
-                      />
-                        <p>{flag.name}</p>
+                      <a
+                        className="dropdown-item d-flex  align-items-center justify-content-between"
+                        href="#"
+                      >
+                        <div className="d-flex">
+                          <img
+                            className="h-20px me-3"
+                            src="../images/moon.png"
+                            alt=""
+                          />
+                          <p>Dark Theme</p>
+                        </div>
+                        <div>
+                          <div className="form-check form-switch">
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              role="switch"
+                              id="switchCheckDefault"
+                            />
+                          </div>
+                        </div>
                       </a>
                     </li>
-                  ))}
-                </ul>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link btn "
-                  type="button"
-                  data-bs-toggle="modal"
-                  data-bs-target="#registerModal"
-                >
-                  Register
-                </button>
-                <div
-                  className="modal fade"
-                  id="registerModal"
-                  tabindex="-1"
-                  aria-labelledby="registerModalLabel"
-                  aria-hidden="true"
-                >
-                  <div className="modal-dialog">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h1
-                          className="modal-title fs-5"
-                          id="registerModalLabel"
-                        >
-                          Modal title
-                        </h1>
-                        <button
-                          type="button"
-                          className="btn-close"
-                          data-bs-dismiss="modal"
-                          aria-label="Close"
-                        ></button>
-                      </div>
-                      <div className="modal-body">...</div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-secondary"
-                          data-bs-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                        <button type="button" className="btn btn-primary">
-                          Save changes
-                        </button>
-                      </div>
+                    <div className="d-lg-none d-md-none d-sm-block">
+                      <NavbarItems></NavbarItems>
                     </div>
-                  </div>
-                </div>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link btn "
-                  type="button"
-                  data-bs-toggle="modal"
-                  data-bs-target="#LoginModal"
-                >
-                  Login
-                </button>
-                <div
-                  className="modal fade"
-                  id="LoginModal"
-                  tabindex="-1"
-                  aria-labelledby="LoginModalLabel"
-                  aria-hidden="true"
-                >
-                  <div className="modal-dialog">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="LoginModalLabel">
-                          Modal title
-                        </h1>
-                        <button
-                          type="button"
-                          className="btn-close"
-                          data-bs-dismiss="modal"
-                          aria-label="Close"
-                        ></button>
-                      </div>
-                      <div className="modal-body">...</div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-secondary"
-                          data-bs-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                        <button type="button" className="btn btn-primary">
-                          Save changes
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu dropdown-menu-end">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
+                    <li>
+                      <a
+                        className="dropdown-item d-flex  align-items-center"
+                        href="#"
+                      >
+                        <img
+                          className="h-20px me-3"
+                          src="../images/documents.png"
+                          alt=""
+                        />
+                        <p>Privacy Policy</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item d-flex  align-items-center"
+                        href="#"
+                      >
+                        <img
+                          className="h-20px me-3"
+                          src="../images/documents.png"
+                          alt=""
+                        />
+                        <p>Terms & Conditions</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item d-flex  align-items-center"
+                        href="#"
+                      >
+                        <img
+                          className="h-20px me-3"
+                          src="../images/documents.png"
+                          alt=""
+                        />
+                        <p>Contact Us</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item d-flex  align-items-center"
+                        href="#"
+                      >
+                        <img
+                          className="h-20px me-3"
+                          src="../images/documents.png"
+                          alt=""
+                        />
+                        <p>Cancellation Policy</p>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </div>
             </ul>
           </div>
         </div>
---
+++ src/components/NavbarItems.jsx
@@ -0,0 +1,82 @@
+import Logo from "./Logo";
+import "./Navbar.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+class FlagList {
+  constructor(name, imgName) {
+    this.name = name;
+    this.imgName = imgName;
+  }
+}
+
+export default function NavbarItems() {
+  const flags = [
+    new FlagList("Hindi", "../images/flag.png"),
+    new FlagList("English", "../images/united-kingdom.png"),
+    new FlagList("Japanese", "../images/japan.png"),
+    new FlagList("French", "../images/france.png"),
+    new FlagList("Spanish", "../images/spain.png"),
+  ];
+  return (
+    <>
+      {/* <div className="d-flex justify-content-between w-100 align-items-center"> */}
+        <li className="nav-item dropdown ms-2 my-1">
+          <a
+            className="nav-link dropdown-toggle d-flex align-items-center ms-2"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img className="h-20px" src="../images/download.png" alt="" />
+            <p className="align-items-center d-flex ms-3">Download</p>
+          </a>
+          <div className="dropdown-menu downloadDropdown rounded-4 p-2">
+            <div className="row g-0 h-100 w-100">
+              <div className="col-6 h-100 d-flex flex-column p-2  justify-content-center">
+                <p className="mb-2 center-div">Download App</p>
+                <div className="d-flex flex-column justify-content-between h-60">
+                  <img className="h-30px w-100" src="../images/as.png" alt="" />
+                  <img className="h-30px w-100" src="../images/ps.png" alt="" />
+                </div>
+              </div>
+              <div className="col-6 d-flex flex-column p-2  justify-content-center">
+                <p className="mb-2 center-div">Scan Code</p>
+                <img
+                  className="h-60"
+                  src="../images/dummyQR.png"
+                  alt="download"
+                />
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="nav-item dropdown ms-2 my-1">
+          <a
+            className="nav-link dropdown-toggle d-flex align-items-center ms-2"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img className="h-20px" src="../images/earth.png" alt="" />
+            <p className="align-items-center d-flex ms-3">Languages</p>
+          </a>
+          <ul className="dropdown-menu">
+            {flags.map((flag, index) => (
+              <li>
+                <a
+                  className="dropdown-item d-flex  align-items-center"
+                  href="#"
+                >
+                  <img className="h-20px me-3" src={flag.imgName} alt="" />
+                  <p>{flag.name}</p>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </li>
+        
+      {/* </div> */}
+    </>
+  );
+}
